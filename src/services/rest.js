9f3c00b167ec0d75aa0c2d6a15327f218ad648d3
giacomo <mariang1@aalto.fi>
2019-12-28 22:31:49 +0100

    added unit tests for utils.js

--- src/services/rest.js
+++ src/services/rest.js
@@ -4,14 +4,11 @@ const baseUrl = 'https://rata.digitraffic.fi/api/v1/'
 // Date should be given in format 'YYYY-MM-DD'
 export const getAllTrainsByDate = async (date) => {
   const response = await axios.get(baseUrl + 'trains/' + date);
-  // console.log('All trains by date', response.data);
   return response.data;
 }
 
-// Stations list from https://rata.digitraffic.fi/api/v1/metadata/stations
 export const getActiveTrains = async (station) => {
   const response = await axios.get(baseUrl + 'live-trains/station/' + station);
-  // console.log('Active trains by station', response.data);
   return response.data;
 }
 
@@ -19,7 +16,6 @@ export const getActiveTrains = async (station) => {
 export const getTrainLocation = async (train) => {
   const response = await axios.get(baseUrl + '' +
       'train-locations/latest/' + train);
-  // console.log('Train location', response.data);
   return response;
 }
 
@@ -27,13 +23,4 @@ export const getTrainLocation = async (train) => {
 export const getStationMetadata = async () => {
   const response = await axios.get(baseUrl + '/metadata/stations');
   return response.data;
-}
-
-
-// Returns the train locations of the latestes trains within a bounding box
-export const getTrainLocationsWithinBoundingBox = async (bounding_box) => {
-    const response = await axios.get(baseUrl + 'train-locations/latest?' + bounding_box);
-    // console.log('Send rest api request', baseUrl + 'train-locations/latest?' + bounding_box);
-    // console.log('Train locations withing bounding box', response.data);
-    return response.data;
 }
